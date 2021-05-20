df48bf1a8e4235b32c3f537a9cfd6a6e3c2bd019
Bruno César Oliveira Dias <brunocesarodias@gmail.com>
2021-05-20 20:44:04 -0300

    Implementação do CRUD com entity

---
+++ CrudComEFCore/CrudComEntityCore/CrudComEntityCore/DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using CrudComEntityCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudComEntityCore
+{
+    public class DataBaseContext: DbContext
+    {
+        public DataBaseContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public IConfiguration Configuration { get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration["ConnectionString"]);
+        }
+    }
+}
---
+++ CrudComEFCore/CrudComEntityCore/CrudComEntityCore/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudComEntityCore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Autores")]
+        [Required]
+        public string Authors { get; set; }
+
+        [Display(Name = "Gênero")]
+        [Required]
+        public string Gender { get; set; }
+
+        [Display(Name = "Quantidade de páginas")]
+        [Required]
+        public int PageCount { get; set; }
+    }
+}
